2ee1871f4548c2641bca20fe47c3d350f10268f2
nttnhannho <makelifeperfect@gmail.com>
2021-09-26 16:30:10 +0700

    Add static array

--- static_array.py
+++ static_array.py
@@ -3,77 +3,100 @@ from abc import ABC, abstractmethod
 
 class StaticArrayADT(ABC):
     @abstractmethod
-    def is_empty(self):
+    def __len__(self):
+        """
+        Length of array
+        """
         pass
 
     @abstractmethod
-    def get(self, index_):
+    def __str__(self):
+        """
+        Print array
+        """
         pass
 
     @abstractmethod
-    def index_of(self, value_):
-        pass
-
-    @abstractmethod
-    def contains(self, value_):
+    def __getitem__(self, index_):
+        """
+        Fetch an item by index
+        """
         pass
 
     @abstractmethod
     def clear(self):
+        """
+        Clear array
+        """
+        pass
+
+    @abstractmethod
+    def search(self, item_):
+        """
+        Search index of an item
+        """
+        pass
+
+    @abstractmethod
+    def is_empty(self):
+        """
+        Check if array is empty
+        """
+        pass
+
+    @abstractmethod
+    def contains(self, item_):
+        """
+        Check if array contains given item
+        """
         pass
 
 
 class StaticArray(StaticArrayADT):
-    def __init__(self, *args, type_=int):
-        self.__size = len(list(map(type_, [*args])))
-        self.__items = [*args]
+    def __init__(self, *args):
+        self.__n = len([*args])
+        self.__arr = [*args]
+
+    def __len__(self):
+        return self.__n
 
     def __str__(self):
-        return f"[{', '.join([str(i) for i in self.__items])}]"
+        return f"{[self.__arr[i] for i in range(self.__n)]}"
 
-    def size(self):
-        return self.__size
-
-    def is_empty(self):
-        return self.__size == 0
-
-    def get(self, index_):
-        return self.__items[index_]
-
-    def index_of(self, value_):
-        return self.__items.index(value_)
-
-    def contains(self, value_):
-        return value_ in self.__items
+    def __getitem__(self, index_):
+        if index_ < 0 or index_ > self.__n:
+            return "Index Error"
+        return self.__arr[index_]
 
     def clear(self):
-        self.__items = [None] * self.__size
+        self.__n = 0
+
+    def search(self, item_):
+        for i in range(self.__n):
+            if item_ == self.__arr[i]:
+                return i
+        print("Not Found")
+        return -1
+
+    def is_empty(self):
+        return self.__n == 0
+
+    def contains(self, item_):
+        return item_ in self.__arr
 
 
 if __name__ == "__main__":
-    int_arr = StaticArray(1, 2, 3, 4, 5)
-    print(int_arr)
-    print(int_arr.size())
-    print(int_arr.is_empty())
-
-    print(int_arr.get(0))
-    print(int_arr.get(1))
-    print(int_arr.get(2))
-    print(int_arr.get(3))
-    print(int_arr.get(4))
-
-    print(int_arr.index_of(1))
-    print(int_arr.index_of(2))
-    print(int_arr.index_of(3))
-    print(int_arr.index_of(4))
-    print(int_arr.index_of(5))
-
-    print(int_arr.contains(6))
-    print(int_arr.contains(5))
-
-    # print(int_arr.get(5))
-    # print(int_arr.index_of(6))
-
-    int_arr.clear()
-    print(int_arr)
-    print(int_arr.size())
+    arr = StaticArray(1, 2, 3, 4, 5)
+    print(arr)
+    print(len(arr))
+    print(arr.is_empty())
+    print(arr[0])
+    print(arr[100])
+    print(arr.contains(1))
+    print(arr.contains(100))
+    print(arr.search(5))
+    print(arr.search(100))
+    arr.clear()
+    print(arr)
+    print(len(arr))
+    print(arr.is_empty())
