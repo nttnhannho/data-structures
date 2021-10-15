0cc67cf4f37a9a233a9f4c746cfac772ea42b243
nttnhannho <makelifeperfect@gmail.com>
2021-10-15 21:23:01 +0700

    MISC

--- user_defined_data_structures/array/dynamic_array.py
+++ user_defined_data_structures/array/dynamic_array.py
@@ -2,6 +2,22 @@ from abc import ABC, abstractmethod
 import ctypes
 
 
+class EmptyArrayError(Exception):
+    def __init__(self, msg_):
+        self.__msg = msg_
+
+    def __str__(self):
+        return f"{self.__msg}"
+
+
+class ElementNotFoundError(Exception):
+    def __init__(self, msg_):
+        self.__msg = msg_
+
+    def __str__(self):
+        return f"{self.__msg}"
+
+
 class DynamicArrayADT(ABC):
     @abstractmethod
     def __len__(self):
@@ -112,6 +128,10 @@ class DynamicArray(DynamicArrayADT):
         self.__capacity = 1
         self.__arr = DynamicArray.__make_array(self.__capacity)
 
+    @property
+    def capacity(self):
+        return self.__capacity
+
     def __len__(self):
         return self.__size
 
@@ -132,9 +152,7 @@ class DynamicArray(DynamicArrayADT):
         if not self.__is_valid_index(index_):
             raise IndexError("Index error")
 
-        half_capacity = int(self.__capacity / 2)
-        if self.__is_size_le(half_capacity):
-            self.__resize(half_capacity)
+        self.__decrease_size()
 
         for i in range(index_, self.__size - 1):
             self.__arr[i] = self.__arr[i + 1]
@@ -148,8 +166,7 @@ class DynamicArray(DynamicArrayADT):
         return False
 
     def append(self, item_):
-        if self.__is_size_equal(self.__capacity):
-            self.__resize(2 * self.__capacity)
+        self.__increase_size()
 
         self.__arr[self.__size] = item_
         self.__size += 1
@@ -158,8 +175,7 @@ class DynamicArray(DynamicArrayADT):
         if not self.__is_valid_index(index_):
             raise IndexError("Index error")
 
-        if self.__is_size_equal(self.__capacity):
-            self.__resize(2 * self.__capacity)
+        self.__increase_size()
 
         for i in range(self.__size - 1, index_ - 1, -1):
             self.__arr[i + 1] = self.__arr[i]
@@ -168,23 +184,20 @@ class DynamicArray(DynamicArrayADT):
         self.__size += 1
 
     def remove(self, item_):
-        half_capacity = int(self.__capacity / 2)
-        if self.__is_size_le(half_capacity):
-            self.__resize(half_capacity)
-
-        for i in range(self.__size):
-            if self.__arr[i] == item_:
-                for j in range(i, self.__size - 1):
-                    self.__arr[j] = self.__arr[j + 1]
-                self.__size -= 1
-                break
-        else:
-            raise Exception(f"{item_} is not existed")
+        index = self.search(item_)
+        if index == -1:
+            raise ElementNotFoundError(f"{item_} is not existed")
+        self.remove_at(index)
 
     def remove_at(self, index_):
         self.__delitem__(index_)
 
     def pop(self):
+        if self.is_empty():
+            raise EmptyArrayError("Array is empty")
+
+        self.__decrease_size()
+
         temp = self.__size
         self.__size -= 1
         return temp
@@ -205,11 +218,14 @@ class DynamicArray(DynamicArrayADT):
     def __is_valid_index(self, index_):
         return 0 <= index_ < self.__size
 
-    def __is_size_le(self, half_capacity):
-        return self.__size <= half_capacity
+    def __increase_size(self):
+        if self.__size == self.__capacity:
+            self.__resize(2 * self.__capacity)
 
-    def __is_size_equal(self, half_capacity):
-        return self.__size == half_capacity
+    def __decrease_size(self):
+        half_capacity = self.__capacity // 2
+        if self.__size <= half_capacity:
+            self.__resize(half_capacity)
 
     def __resize(self, new_capacity_):
         new_arr = DynamicArray.__make_array(new_capacity_)
