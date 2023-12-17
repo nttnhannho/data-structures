92a6a19d32ceb3e1d6360104223121736484f26b
nttnhannho <vle@m3-tech.ca>
2023-12-18 00:13:20 +0700

    Fix indentation issues

--- bonus/context_manager.py
+++ bonus/context_manager.py
@@ -3,33 +3,33 @@ class FileManager:
         self.file_path = file_path
         self.mode = mode
         self.file = None
-    
+
     def __enter__(self):
         self.file = open(self.file_path, self.mode)
         return self
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
     	if self.file:
-    		self.file.close()
-    
+            self.file.close()
+
     def write(self, text):
     	if self.mode == 'w':
-    		self.file.write(text)
-    
+    	    self.file.write(text)
+
     def read(self):
     	if self.mode == 'r':
-    		for line in self.file:
-    			print(line)
+    	    for line in self.file:
+    	        print(line)
 
 
-def main(): 
-	with FileManager('test.txt', 'w') as f:
-		f.write('Example text\n')
-		f.write('Nhan Nguyen')
+def main():
+    with FileManager('test.txt', 'w') as f:
+        f.write('Example text\n')
+        f.write('Nhan Nguyen')
 
-	with FileManager('test.txt', 'r') as f:
-		f.read()
+    with FileManager('test.txt', 'r') as f:
+        f.read()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- bonus/context_manager_with_decorator.py
+++ bonus/context_manager_with_decorator.py
@@ -3,13 +3,18 @@ from contextlib import contextmanager
 
 @contextmanager
 def manage_file(file_path):
-	file = open(file_path, 'w')
-	try:
-		yield file
-	finally:
-		file.close()
-			
+    file = open(file_path, 'w')
+    try:
+        yield file
+    finally:
+        file.close()
 
-with manage_file('test.txt') as f:
-	f.write('Example text')
+
+def main():
+    with manage_file('test.txt') as f:
+        f.write('Example text')
+
+
+if __name__ == '__main__':
+    main()
 
